8cc23f6b5f79e50fc3e1223373869c3fa25c5868
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-11-05 14:12:58 -0700

    removed quotes from list items

--- Phase2.py
+++ Phase2.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
         #printinfo(empname, hours, hourlyrate, grosspay, taxrate, incometax, netpay)
 
         #write code to insert fromdate, todate, empname, hours, hourlyrate, and taxrate into list EmpDetail
-        EmpDetail = ['fromdate', 'todate', 'empname', 'hours', 'hourlyrate', 'taxrate']
+        EmpDetail = [fromdate, todate, empname, hours, hourlyrate, taxrate]
 
         #the following code appends the list EmpDetail to the list EmpDetailList
         EmpDetailList.append(EmpDetail)
